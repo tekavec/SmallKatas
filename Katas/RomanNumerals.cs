671a4419f85c3e0b0ad3945064d15e492090592e
tekavec <tomaz@codurance.com>
2015-08-24 16:55:02 +0100

    Small katas repository, initially with Roman numerals kata

---
+++ Katas/RomanNumerals.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace Katas
+{
+    public class RomanNumerals
+    {
+        private readonly string[] _RomanStrings = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        private readonly int[] _ArabicValues = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        public string GetRomanFromArabic(int arabic)
+        {
+            var result = new StringBuilder();
+            var remains = arabic;
+            for (int i = 0; i < _RomanStrings.Length; i++)
+            {
+                remains = AppendRomanNumerals(remains, result, _RomanStrings[i], _ArabicValues[i]);
+            }
+            return result.ToString();
+        }
+
+        private static int AppendRomanNumerals(
+            int remains, 
+            StringBuilder result, 
+            string romanNumeral, 
+            int arabic)
+        {
+            while (remains >= arabic)
+            {
+                result.Append(romanNumeral);
+                remains -= arabic;
+            }
+            return remains;
+        }
+    }
+}
---
+++ Tests/RomanNumeralsKataTest.cs
@@ -0,0 +1,119 @@
+﻿using Katas;
+using NUnit.Framework;
+
+namespace Tests
+{
+    [TestFixture]
+    public class RomanNumeralsKataTest
+    {
+        private RomanNumerals _RomanNumerals;
+
+        [SetUp]
+        public void Initialize()
+        {
+            _RomanNumerals = new RomanNumerals();
+            
+        }
+    
+
+
+        [Test]
+        public void OneToThree()
+        {
+            Assert.AreEqual("I", _RomanNumerals.GetRomanFromArabic(1));
+            Assert.AreEqual("II", _RomanNumerals.GetRomanFromArabic(2));
+            Assert.AreEqual("III", _RomanNumerals.GetRomanFromArabic(3));
+        }
+
+        [Test]
+        public void Four()
+        {
+            Assert.AreEqual("IV", _RomanNumerals.GetRomanFromArabic(4));
+        }
+
+        [Test]
+        public void Five()
+        {
+            Assert.AreEqual("V", _RomanNumerals.GetRomanFromArabic(5));
+        }
+
+        [Test]
+        public void Six()
+        {
+            Assert.AreEqual("VI", _RomanNumerals.GetRomanFromArabic(6));
+        }
+
+        [Test]
+        public void Nine()
+        {
+            Assert.AreEqual("IX", _RomanNumerals.GetRomanFromArabic(9));
+        }
+
+        [Test]
+        public void Ten()
+        {
+            Assert.AreEqual("X", _RomanNumerals.GetRomanFromArabic(10));
+        }
+
+        [Test]
+        public void Twenty()
+        {
+            Assert.AreEqual("XX", _RomanNumerals.GetRomanFromArabic(20));
+        }
+
+        [Test]
+        public void Thirty()
+        {
+            Assert.AreEqual("XXX", _RomanNumerals.GetRomanFromArabic(30));
+        }
+
+        [Test]
+        public void Fourty()
+        {
+            Assert.AreEqual("XL", _RomanNumerals.GetRomanFromArabic(40));
+        }
+
+        [Test]
+        public void Fifty()
+        {
+            Assert.AreEqual("L", _RomanNumerals.GetRomanFromArabic(50));
+        }
+
+        [Test]
+        public void Ninety()
+        {
+            Assert.AreEqual("XC", _RomanNumerals.GetRomanFromArabic(90));
+        }
+
+        [Test]
+        public void Hundred()
+        {
+            Assert.AreEqual("C", _RomanNumerals.GetRomanFromArabic(100));
+        }
+
+        [Test]
+        public void FourHundred()
+        {
+            Assert.AreEqual("CD", _RomanNumerals.GetRomanFromArabic(400));
+        }
+
+        [Test]
+        public void FiveHundred()
+        {
+            Assert.AreEqual("D", _RomanNumerals.GetRomanFromArabic(500));
+        }
+
+        [Test]
+        public void NineHundred()
+        {
+            Assert.AreEqual("CM", _RomanNumerals.GetRomanFromArabic(900));
+        }
+
+        [Test]
+        public void Thousand()
+        {
+            Assert.AreEqual("M", _RomanNumerals.GetRomanFromArabic(1000));
+        }
+
+    }
+}
